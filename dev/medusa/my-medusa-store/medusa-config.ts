3cf37960fb8b9f6991e0214b6942f31e06b221e3
F.K.A. Soffers <diorcula@duck.com>
2025-03-17 10:53:03 +0100

    Docker fix flow

--- dev/medusa/my-medusa-store/medusa-config.ts
+++ dev/medusa/my-medusa-store/medusa-config.ts
@@ -4,13 +4,15 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl:
+      process.env.DATABASE_URL ||
+      'postgres://postgres:mysecretpassword@postgres:5432/medusa-my-medusa-store',
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
 })
